02e0d206d79ac2e5f5a1e96c3eca025fb8c978a7
Miguel Alejo <miguel.sebastian.alejo@gmail.com>
2024-12-08 18:47:12 -0700

    added progress bar and looped alarm until stopped

--- countdown.ps1
+++ countdown.ps1
@@ -1,14 +1,16 @@
 function Countdown-Timer {
-  param(
+param(
     [int]$Seconds = 60,
-    [string]$AlarmSound = "D:\code\personal\terminal-timer-script\alarm-sounds\lamp_3.wav"
-  )
+    [string]$AlarmSound = "./alarm-sounds/lamp_3.wav"
+    )
 
+  # check if the alarm sound file exists
   if (!(Test-Path -Path $AlarmSound)) {
     Write-Error "The specified alarm sound file '$AlarmSound' does not exist."
     return
   }
 
+  # check for corret file type
   $supportedExtensions = @(".wav")
   $fileExtension = [System.IO.Path]::GetExtension($AlarmSound).ToLower()
   if ($fileExtension -notin $supportedExtensions) {
@@ -16,18 +18,32 @@ function Countdown-Timer {
     return
   }
 
+  # calculate time
   $startTime = [DateTime]::Now
   $endTime = $startTime.AddSeconds($Seconds)
 
-  while ($endTime -gt [DateTime]::Now) {
+  # countdown timer and progress bar
+  Write-Host "Countdown Timer Started:"
+  for($i = 0; $i -lt $Seconds; $i++) {
     $remainingTime = $endTime - [DateTime]::Now
     $secondsLeft = [int]$remainingTime.TotalSeconds
-    Write-Host -NoNewLine "Time remaining: $secondsLeft seconds `r"
+    $progress = [math]::Round(($i + 1) / $Seconds * 100)
+    Write-Progress -Activity "Counting Down" -Status "$secondsLeft seconds left" -PercentComplete $progress
     Start-Sleep -Seconds 1
   }
 
   Write-Host "`nTime's up!"
-  [System.Media.SoundPlayer]$player = New-Object System.Media.SoundPlayer
-  $player.SoundLocation = $AlarmSound
-  $player.PlaySync()
+
+  # loop alarm sound
+  try {
+    [System.Media.SoundPlayer]$player = New-Object System.Media.SoundPlayer
+    $player.SoundLocation = $AlarmSound
+    Write-Host "Playing alarm. Press Ctrl+C to stop."
+
+    while ($true) {
+      $player.PlaySync()
+    }
+  } catch {
+    Write-Error "Failed to play the alarm sound: $_"
+  }
 }
